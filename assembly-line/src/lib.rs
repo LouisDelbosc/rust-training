38c2f71591c2c7a03ef85ac7c6b0b5253d9f6784
Louis Delbosc <louisdelbosc@gmail.com>
2022-01-11 18:14:09 +0100

    Exercism 3 done

---
+++ assembly-line/src/lib.rs
@@ -0,0 +1,17 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    let production = f64::from(speed) * 221.0;
+    match speed {
+        0..=4 => production,
+        5..=8 => production * 0.9,
+        9..=10 => production * 0.77,
+        _ => panic!("Unexpected speed {}", speed),
+    }
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    production_rate_per_hour(speed) as u32 / 60
+}

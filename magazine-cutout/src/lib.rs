2ba18209114bd370fc4ae8198ab19b2ecab203dc
Louis Delbosc <louisdelbosc@gmail.com>
2022-01-14 17:11:34 +0100

    Exercism 11 done

---
+++ magazine-cutout/src/lib.rs
@@ -0,0 +1,20 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+use std::collections::HashMap;
+
+pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
+    let magazine_words = magazine.iter().fold(HashMap::new(), |mut words, word| {
+        *words.entry(word).or_insert(0) += 1;
+        words
+    });
+    let note_words = note.iter().fold(HashMap::new(), |mut words, word| {
+        *words.entry(word).or_insert(0) += 1;
+        words
+    });
+
+    note_words
+        .iter()
+        .all(|(w, count)| magazine_words.get(w).unwrap_or(&0) >= count)
+}
